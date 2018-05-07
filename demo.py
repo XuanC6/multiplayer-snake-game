73edd7008d8b5b23178fec3a35e50f7022283239
Xuan Chen <33044517+XuanC6@users.noreply.github.com>
2018-05-07 07:00:53 -0400

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,19 @@
+import gym
+import time
+
+env = gym.make('MultiSnake-v0')
+
+for i in range(100):
+    obs = env.reset()
+    env.render()
+    time.sleep(0.15)
+
+    for t in range(1000):
+        actions = env.action_space.sample()
+        obs, reward, dones, info = env.step(actions)
+        env.render()
+        time.sleep(0.15)
+
+        if all(dones):
+            print('episode {} finished after {} timesteps'.format(i, t))
+            break
