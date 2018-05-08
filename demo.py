6a62e7073e703ddbbccccc739310cbd2b2e57530
XuanC6 <33044517+XuanC6@users.noreply.github.com>
2018-05-08 16:10:20 -0400

    sleep time

--- demo.py
+++ demo.py
@@ -6,13 +6,13 @@ env = gym.make('MultiSnake-v0')
 for i in range(100):
     obs = env.reset()
     env.render()
-    time.sleep(0.15)
+    time.sleep(0.5)
 
     for t in range(1000):
         actions = env.action_space.sample()
         obs, reward, dones, info = env.step(actions)
         env.render()
-        time.sleep(0.15)
+        time.sleep(0.5)
 
         if all(dones):
             print('episode {} finished after {} timesteps'.format(i, t))
